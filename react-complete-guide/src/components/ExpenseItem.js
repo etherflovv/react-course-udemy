9292313e5fc70b3a68b65dddf8e3ab72d1e9344e
etherflovv <107205380+etherflovv@users.noreply.github.com>
2022-06-22 20:46:55 +0800

    refactor: :recycle: split component: extract Expenses into separate component

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -2,8 +2,6 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-
-    
     return (
         <div className="expense-item">
             <ExpenseDate date={props.date} />
