17f068b9ac2d82d8a92e2506a66d2745881f09fe
etherflovv <107205380+etherflovv@users.noreply.github.com>
2022-06-23 09:45:47 +0800

    feat: :sparkles: add filter dropdown for year, event listener and useState for selected year

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+
 import './Expenses.css'
 
 function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2022');
+    
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+
     return (
+        <div>
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler} />
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
@@ -26,6 +37,7 @@ function Expenses(props) {
                 date={props.items[3].date}
             />
         </Card>
+        </div>
     )
 }
 
